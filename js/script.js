7307eecac8c06ecc626422986907746ad6739b2e
GOVARDAN-N-A <e1215024govardancse@gmail.com>
2023-09-03 21:30:23 +0530

    Js File Added

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function()
+{
+$('#containerSlider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    });
+});
+
+
+$(document).ready(function()
+{
+$('#containerSlider2').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    });
+});
+
+
