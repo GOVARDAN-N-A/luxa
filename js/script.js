d7da91a3d1d7f45df71994946c9772b0dca82e6f
GOVARDAN-N-A <e1215024govardancse@gmail.com>
2023-09-04 07:02:02 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-$(document).ready(function()
-{
-$('#containerSlider').slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    });
-});
+// $(document).ready(function()
+// {
+// $('#containerSlider').slick({
+//     dots: true,
+//     infinite: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     });
+// });
 
 
 // $(document).ready(function()
