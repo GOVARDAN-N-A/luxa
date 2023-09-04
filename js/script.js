938e8427d5cfb1bda7be517bb02947e98d199877
GOVARDAN-N-A <e1215024govardancse@gmail.com>
2023-09-04 06:37:20 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -11,16 +11,16 @@ $('#containerSlider').slick({
 });
 
 
-$(document).ready(function()
-{
-$('#containerSlider2').slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    });
-});
+// $(document).ready(function()
+// {
+// $('#containerSlider2').slick({
+//     dots: true,
+//     infinite: true,
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     autoplay: false,
+//     autoplaySpeed: 2000,
+//     });
+// });
 
 
