1894358123bebf4b0bcda9b55a59eb9dd78bcc06
GOVARDAN-N-A <e1215024govardancse@gmail.com>
2023-09-05 22:44:43 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,26 +1,48 @@
-// $(document).ready(function()
-// {
-// $('#containerSlider').slick({
-//     dots: true,
-//     infinite: true,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-//     });
-// });
+$(document).ready(function () {
+    // Slick carousel for containerSlider
+    $('#containerSlider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 
+    $('#containerSlider2').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768, 
+                settings: {
+                    slidesToShow: 2, 
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+    $('.prev').click(function () {
+        $('#containerSlider2').slick('slickPrev');
+    });
 
-// $(document).ready(function()
-// {
-// $('#containerSlider2').slick({
-//     dots: true,
-//     infinite: true,
-//     slidesToShow: 4,
-//     slidesToScroll: 1,
-//     autoplay: false,
-//     autoplaySpeed: 2000,
-//     });
-// });
+    $('.next').click(function () {
+        $('#containerSlider2').slick('slickNext');
+    });
 
-
+});
